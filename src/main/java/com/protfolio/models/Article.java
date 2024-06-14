20ae62b4884f00a11f80a352295b6a4124c0905d
Sawada11 <c0169@oic.jp>
2024-06-14 10:19:12 +0900

    Articleのエンティティを作成

---
+++ src/main/java/com/protfolio/models/Article.java
@@ -0,0 +1,39 @@
+package com.protfolio.models;
+
+import java.util.Date;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "articles")
+public class Article {
+	
+	@OneToMany(mappedBy = "article")
+	 private List<Comment> comment;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	 @ManyToOne
+	 private User user;
+	 
+	 private String title;
+	 
+	 private String content;
+	 
+	 private String imageFileName;
+	 
+	 private Date createdAt;
+	 
+	 
+}
