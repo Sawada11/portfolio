afce1bc23420f37639716d801cbe8259fdeac351
Sawada11 <c0169@oic.jp>
2024-06-14 10:21:02 +0900

    CommentDtoを作成

---
+++ src/main/java/com/protfolio/dto/CommentDto.java
@@ -0,0 +1,16 @@
+package com.protfolio.dto;
+
+import com.protfolio.models.Article;
+import com.protfolio.models.User;
+
+import jakarta.validation.constraints.NotEmpty;
+
+public class CommentDto {
+	
+	private Article article;
+	
+	private User user;
+	
+	 @NotEmpty(message = "名前が入力してください") 
+	private String content;
+}
