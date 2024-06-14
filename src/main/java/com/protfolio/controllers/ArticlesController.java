ccc3e4dd3315ba4f2138f2d27d87b88a0325ab0a
Sawada11 <c0169@oic.jp>
2024-06-14 10:25:02 +0900

    first

---
+++ src/main/java/com/protfolio/controllers/ArticlesController.java
@@ -0,0 +1,27 @@
+package com.protfolio.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.protfolio.models.Article;
+import com.protfolio.repository.ArticlesRepository;
+
+@Controller
+@RequestMapping("/articles")
+public class ArticlesController {
+
+	@Autowired
+	private ArticlesRepository repo;
+	
+	@GetMapping({"", "/"})
+	public String showArticleList(Model model) {
+		List<Article> articles = repo.findAll(Sort.by(Sort.Direction.DESC, "id"));
+		
+	}
+}
