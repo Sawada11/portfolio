43de45f87c1fc1fbbac53d3acd4f204fa9b49a2a
Sawada11 <c0169@oic.jp>
2024-06-19 14:32:18 +0900

    記事の投稿機能

--- src/main/java/com/protfolio/controllers/ArticlesController.java
+++ src/main/java/com/protfolio/controllers/ArticlesController.java
@@ -1,17 +1,30 @@
 package com.protfolio.controllers;
 
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.protfolio.dto.ArticleDto;
 import com.protfolio.models.Article;
 import com.protfolio.repository.ArticlesRepository;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/articles")
@@ -32,12 +45,62 @@ public class ArticlesController {
 	}
 	
 	/*
-	 * 保存ページを表示
+	 * 投稿ページを表示
 	 */
 	@GetMapping("/create")
 	public String showCreatePage(Model model) {
 		ArticleDto articleDto = new ArticleDto();
 		model.addAttribute("articleDto", articleDto);
+
 		return "articles/create";
+	}
+	
+	/*
+	 * 投稿機能
+	 */
+	@PostMapping("/create")
+	public String createProduct(
+			@Valid @ModelAttribute ArticleDto articleDto,
+			BindingResult result) {
+
+		if(articleDto.getImageFileName().isEmpty()) {
+			result.addError(new FieldError("articleDto","imageFileName","画像が挿入されていません。"));
+		}
+		
+		if(result.hasErrors()) {
+			return "articles/create";
+		}
+
+		MultipartFile image = articleDto.getImageFileName();
+		Date createAt = new Date();
+//		 作成時刻と元のファイル名を組み合わせて、一意の保存ファイル名を生成
+		String storageFileName = createAt.getTime() + "_" + image.getOriginalFilename();
+		
+		try {
+			String uploadDir = "public/images/";
+			Path uploadPath = Paths.get(uploadDir); 
+			if(!Files.exists(uploadPath)) {
+				Files.createDirectories(uploadPath);
+			}
+//			画像データを指定のディレクトリにコピー
+			try (InputStream inputStream = image.getInputStream()){
+				Files.copy(inputStream, Paths.get(uploadDir + storageFileName),
+							StandardCopyOption.REPLACE_EXISTING);
+			}
+			
+		}catch(Exception e){
+			System.out.println("例外:" + e.getMessage());
+		}
+		
+		Article article = new Article();
+		article.setUser(articleDto.getUser());
+		article.setTitle(articleDto.getTitle());
+		article.setContent(articleDto.getContent());
+		article.setImageFileName(storageFileName);
+		article.setCreatedAt(createAt);
+		
+		repo.save(article);
+		
+		return "redirect:/articles";
 	}
 }
--- src/main/java/com/protfolio/dto/ArticleDto.java
+++ src/main/java/com/protfolio/dto/ArticleDto.java
@@ -4,17 +4,19 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.protfolio.models.User;
 
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Data
 public class ArticleDto {
 	
+	 @ManyToOne
 	 private User user;
-	 @NotEmpty(message = "名前が入力してください") 
+	 
+//	 @NotEmpty(message = "名前が入力してください") 
 	 private String title;
 	 
-	 @NotEmpty(message = "名前が入力してください")
+//	 @NotEmpty(message = "名前が入力してください")
 	 private String content;
 	 
 	 private MultipartFile imageFileName;
