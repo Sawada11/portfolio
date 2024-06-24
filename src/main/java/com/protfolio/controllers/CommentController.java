0e05aa06fd99ee6ebbfbd1819e529ed7144f451b
Sawada11 <c0169@oic.jp>
2024-06-24 14:36:02 +0900

    コメントの投稿

--- src/main/java/com/protfolio/controllers/CommentController.java
+++ src/main/java/com/protfolio/controllers/CommentController.java
@@ -1,16 +1,27 @@
 package com.protfolio.controllers;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.protfolio.dto.ArticleDto;
+import com.protfolio.dto.CommentDto;
 import com.protfolio.models.Article;
+import com.protfolio.models.Comment;
 import com.protfolio.repository.ArticlesRepository;
 import com.protfolio.repository.CommentRepository;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/articles")
@@ -18,7 +29,7 @@ public class CommentController {
 
 	@Autowired
 	private ArticlesRepository repoArticle;
-	
+
 	@Autowired 
 	private CommentRepository repoComment;
 	
@@ -27,6 +38,12 @@ public class CommentController {
 	 */
 	@GetMapping("/comment")
 	public String showCommentPage(Model model, @RequestParam int id) {
+		CommentDto commentDto = new CommentDto();
+		model.addAttribute("commentDto", commentDto);
+		
+//		コメント欄
+		List<Comment> comments = repoComment.findAll(Sort.by(Sort.Direction.DESC, "id"));
+		model.addAttribute("comments",comments);
 		try {
 			Article article = repoArticle.findById(id).get();
 			model.addAttribute("article", article);
@@ -47,18 +64,22 @@ public class CommentController {
 	/*
 	 * コメント送信
 	 */
-//	@PostMapping("/create")
-//	public String showCreateComment(@Valid @ModelAttribute CommentDto commentDto
-//			) {
-//		Date createAt = new Date();
-//		Comment comment = new Comment();
-//		comment.setArticle(commentDto.getArticle());
-//		comment.setUser(commentDto.getUser());
-//		comment.setContent(commentDto.getContent());
-//		comment.setCreatedAt(createAt);
-//		
-//		repoComment.save(comment);
-//		
-//		return "redirect:/comment";
-//	}
+	@PostMapping("/post")
+	public String showPostComment(
+			Model model,
+			@RequestParam int id,
+			@Valid @ModelAttribute CommentDto commentDto,
+			BindingResult result
+			) {
+		Date createAt = new Date();
+		Comment comment = new Comment();
+		comment.setArticle(commentDto.getArticle());
+		comment.setUser(commentDto.getUser());
+		comment.setContent(commentDto.getContent());
+		comment.setCreatedAt(createAt);
+		
+		repoComment.save(comment);
+//	      return "redirect:/articles/" + id;
+		return "redirect:/articles/comment?id=" + id;
+	}
 }
