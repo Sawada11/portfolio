e747fde37b675121d7bc5c664ec1ee3edb64a502
Sawada11 <c0169@oic.jp>
2024-07-09 17:56:20 +0900

    コメントを削除

--- src/main/java/com/protfolio/admin/controller/AdminController.java
+++ src/main/java/com/protfolio/admin/controller/AdminController.java
@@ -170,7 +170,6 @@ public class AdminController {
      */
     @PostMapping("/deleteUser")
     public String deleteUser(@RequestParam Long id) {
-    	System.out.println("\\\\\6666&&&\\\\");
     	User user = userRepo.findById(id).get();
     	userRepo.delete(user);
     	return "redirect:/admin";
