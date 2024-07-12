b97f77900e8f94f58985454eb8a47b3e0d4a5040
Sawada11 <c0169@oic.jp>
2024-07-12 17:55:27 +0900

    記事IDを取得

--- src/main/java/com/protfolio/repository/CommentRepository.java
+++ src/main/java/com/protfolio/repository/CommentRepository.java
@@ -1,9 +1,11 @@
 package com.protfolio.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.protfolio.models.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Integer>{
-
+	List<Comment> findByArticleId(int articleId);
 }
